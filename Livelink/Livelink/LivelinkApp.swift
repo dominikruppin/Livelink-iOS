c6342af3abae0a72fe0e787ebbe6bc563c3f6b98
Dominik Ruppin <>
2024-10-28 21:04:15 +0100

    Projekt angelegt.

---
+++ Livelink/Livelink/LivelinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LivelinkApp.swift
+//  Livelink
+//
+//  Created by Dominik Ruppin on 28.10.24.
+//
+
+import SwiftUI
+
+@main
+struct LivelinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Livelink/LivelinkTests/LivelinkTests.swift
@@ -0,0 +1,17 @@
+//
+//  LivelinkTests.swift
+//  LivelinkTests
+//
+//  Created by Dominik Ruppin on 28.10.24.
+//
+
+import Testing
+@testable import Livelink
+
+struct LivelinkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
