a47ba0b305c0692ea0503db5862d6def2de6b2e1
Dominik Ruppin <>
2024-11-19 16:59:59 +0100

    Sicherheitsstand

--- Livelink/Livelink/Views/Subviews/UserListView.swift
+++ Livelink/Livelink/Views/Subviews/UserListView.swift
@@ -9,41 +9,102 @@ import SwiftUI
 
 struct UserListView: View {
     @EnvironmentObject var channelsViewModel: ChannelsViewModel
+    @EnvironmentObject var userViewModel: UserViewModel
     @Binding var isUserListVisible: Bool
-
+    
     var body: some View {
         HStack {
             Spacer()
-            VStack {
+            
+            VStack(alignment: .leading) {
                 Text("Nutzer im Channel")
                     .font(.headline)
                     .padding()
                     .background(Color.blue)
                     .foregroundColor(.white)
-
+                
                 ScrollView {
-                    VStack(alignment: .leading) {
-                        ForEach(channelsViewModel.onlineUsers, id: \.username) { user in
-                            Text(user.username)
-                                .font(.headline)
-                                .padding(.vertical, 5)
-                                .foregroundColor(.white)
-                        }
-                    }
-                    .padding()
+                    UserList(users: channelsViewModel.onlineUsers)
                 }
-
-                Spacer()
             }
-            .frame(width: 250)  // Breite der Benutzerliste
+            .frame(width: 250)
             .background(Color.black.opacity(0.5))
             .cornerRadius(10)
             .transition(.move(edge: .trailing))
             .animation(.easeInOut, value: isUserListVisible)
-            .onTapGesture {
-                // Verhindert, dass das Panel geschlossen wird, wenn der Benutzer auf die Benutzerliste klickt
+        }
+        .sheet(isPresented: $userViewModel.showProfilePopup) {
+            if let profileData = userViewModel.profileUserData {
+                ProfileViewPopup(profile: profileData)
+                    .background(
+                        Image("background")
+                            .resizable()
+                            .scaledToFill()
+                            .edgesIgnoringSafeArea(.all)
+                    )
+                    .edgesIgnoringSafeArea(.all)
             }
         }
     }
 }
 
+struct UserList: View {
+    let users: [OnlineUser]
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            ForEach(users, id: \.username) { user in
+                UserRow(user: user)
+            }
+        }
+        .padding([.top, .horizontal])
+    }
+}
+
+struct UserRow: View {
+    let user: OnlineUser
+    @EnvironmentObject var userViewModel: UserViewModel
+    
+    var body: some View {
+        HStack(spacing: 10) {
+            // Profilbild
+            AsyncImage(url: URL(string: user.profilePic)) { image in
+                image.resizable()
+                    .scaledToFit()
+                    .frame(width: 30, height: 30)
+                    .clipShape(Circle())
+            } placeholder: {
+                Color.gray.frame(width: 30, height: 30)
+                    .clipShape(Circle())
+            }
+            
+            VStack(alignment: .leading) {
+                HStack {
+                    Text(user.username)
+                        .font(.headline)
+                        .foregroundColor(.white)
+
+                    if user.gender == "Männlich" {
+                        Text("👨")
+                            .foregroundColor(.white)
+                    } else if user.gender == "Weiblich" {
+                        Text("👩")
+                            .foregroundColor(.white)
+                    }
+                }
+                
+                HStack(spacing: 5) {
+                    Text("\(user.age) Jahre")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+            }
+            .padding(.leading, 0)
+        }
+        .padding(.vertical, 5)
+        .onTapGesture {
+            // User Profil öffnen
+            userViewModel.loadUserDataByUsername(username: user.username)
+        }
+    }
+}
